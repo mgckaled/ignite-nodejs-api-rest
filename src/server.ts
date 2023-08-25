b0d4c9509901a28b180ba7cda1abceb1f435c37b
mgckaled <marcelgckaled@gmail.com>
2023-08-25 13:22:30 -0300

    feat: create globlal hook

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,11 @@ import { transactionsRoutes } from './routes/transactions'
 const app = fastify()
 
 app.register(cookie)
+
+app.addHook('preHandler', async (request, reply) => {
+  console.log(`[${request.method}] ${request.url} \n ${reply}`)
+})
+
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
